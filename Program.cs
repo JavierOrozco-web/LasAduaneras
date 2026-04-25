28f21e06e70f87dd6cafb63399fa420172d84ac4
JavierOrozco-web <Javier_OGamez09@outlook.com>
2026-04-24 23:57:57 -0500

    login program.cs

--- Program.cs
+++ Program.cs
@@ -61,6 +61,45 @@ app.MapGet("/productos", async (int? categoriaID, AppDbContext db) =>
 
     return resultado;
 });
+
+app.MapPost("/login", async (LoginRequest req, AppDbContext db) =>
+{
+    // 🔍 Buscar en CLIENTES
+    var cliente = await db.Clientes
+        .FirstOrDefaultAsync(u => 
+            u.Correo == req.Correo && 
+            u.Password == req.Password);
+
+    if (cliente != null)
+    {
+        return Results.Ok(new
+        {
+            tipo = "cliente",
+            id = cliente.ClienteID,
+            nombre = cliente.NombreCompleto
+        });
+    }
+
+    // 🔍 Buscar en EMPLEADOS
+    var empleado = await db.Empleados
+        .FirstOrDefaultAsync(u => 
+            u.Correo == req.Correo && 
+            u.Password == req.Password);
+
+    if (empleado != null)
+    {
+        return Results.Ok(new
+        {
+            tipo = "empleado",
+            id = empleado.EmpleadoID,
+            nombre = empleado.Nombre
+        });
+    }
+
+    // ❌ No encontrado
+    return Results.Unauthorized();
+});
+
 app.MapGet("/categorias", async (AppDbContext db) =>
 {
     return await db.Categorias.ToListAsync();
